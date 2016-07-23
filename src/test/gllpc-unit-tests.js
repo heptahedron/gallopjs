f394842282537c0a3d274354704d52b838946f41
Braxton Spence <brax.spence@gmail.com>
2016-07-22 20:28:51 -0500

    Add more tests

--- src/test/gllpc-unit-tests.js
+++ src/test/gllpc-unit-tests.js
@@ -21,7 +21,24 @@ describe('gllpc', function() {
           it: 'should not consume more than necessary' },
         { p: bird, str: 'notbird', fail: { rem: 'notbird' },
           it: 'should fail to parse an incorrect string' },
+        { p: empty, str: '', succ: { val: '', rem: '' },
+          it: 'should succeed in parsing the empty string' },
+        { p: empty, str: 'extra', succ: { val: '', rem: 'extra' },
+          it: 'should not consume anything when parsing the empty string' },
       ] },
+    { pClass: gllpc.TerminalSequentialParser,
+      tests: [
+        { p: simpleSeq, str: 'birdistheword',
+          succ: { val: ['bird', 'istheword'], rem: '' },
+          it: 'should successfully parse both terminals in sequence' },
+        { p: simpleSeq, str: 'bird', fail: { rem: '' },
+          it: 'should fail to parse a too-short string' },
+        { p: simpleSeq, str: 'istheword', fail: { rem: 'istheword' },
+          it: 'should not forget to parse the first terminal' },
+        { p: simpleSeq, str: '', fail: { rem: '' },
+          it: 'should properly handle the empty string' },
+      ] },
+    // TODO more tests
   ]
 
   basicExpectations.forEach(({ pClass, tests }) => {
@@ -35,7 +52,7 @@ describe('gllpc', function() {
           res.should.be.instanceof(shouldSucceed
                                    ? gllpc.Success
                                    : gllpc.Failure)
-          expRes.val && res.val.should.equal(expRes.val)
+          expRes.val && res.val.should.deep.equal(expRes.val)
           expRes.rem && res.rem.should.equal(expRes.rem)
         })
       })
--- test/all-tests.js
+++ test/all-tests.js
@@ -73,7 +73,15 @@
 	    tests: [{ p: bird, str: 'bird', succ: { val: 'bird', rem: '' },
 	      it: 'should succeed and consume the whole string' }, { p: bird, str: 'birdextra', succ: { val: 'bird', rem: 'extra' },
 	      it: 'should not consume more than necessary' }, { p: bird, str: 'notbird', fail: { rem: 'notbird' },
-	      it: 'should fail to parse an incorrect string' }] }];
+	      it: 'should fail to parse an incorrect string' }, { p: empty, str: '', succ: { val: '', rem: '' },
+	      it: 'should succeed in parsing the empty string' }, { p: empty, str: 'extra', succ: { val: '', rem: 'extra' },
+	      it: 'should not consume anything when parsing the empty string' }] }, { pClass: gllpc.TerminalSequentialParser,
+	    tests: [{ p: simpleSeq, str: 'birdistheword',
+	      succ: { val: ['bird', 'istheword'], rem: '' },
+	      it: 'should successfully parse both terminals in sequence' }, { p: simpleSeq, str: 'bird', fail: { rem: '' },
+	      it: 'should fail to parse a too-short string' }, { p: simpleSeq, str: 'istheword', fail: { rem: 'istheword' },
+	      it: 'should not forget to parse the first terminal' }, { p: simpleSeq, str: '', fail: { rem: '' },
+	      it: 'should properly handle the empty string' }] }];
 
 	  basicExpectations.forEach(function (_ref) {
 	    var pClass = _ref.pClass;
@@ -93,7 +101,7 @@
 	              expRes = succ || fail;
 
 	          res.should.be.instanceof(shouldSucceed ? gllpc.Success : gllpc.Failure);
-	          expRes.val && res.val.should.equal(expRes.val);
+	          expRes.val && res.val.should.deep.equal(expRes.val);
 	          expRes.rem && res.rem.should.equal(expRes.rem);
 	        });
 	      });
